99778471db51fb15733a48996b2f8815cc1cfbc2
baurine <2008.hbl@gmail.com>
2015-07-05 13:36:17 +0800

    add empty UserProfileActivity

---
+++ app/src/main/java/com/baurine/instamaterial/ui/activity/UserProfileActivity.java
@@ -0,0 +1,14 @@
+package com.baurine.instamaterial.ui.activity;
+
+import android.os.Bundle;
+
+import com.baurine.instamaterial.R;
+
+public class UserProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
