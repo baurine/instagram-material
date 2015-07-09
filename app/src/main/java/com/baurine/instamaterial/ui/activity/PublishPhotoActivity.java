42ffd678393d62cd2de717e38cbec91de4ec40be
baurine <2008.hbl@gmail.com>
2015-07-09 14:30:24 +0800

    完成 PublishPhotoActivity toolbar 的布局

--- app/src/main/java/com/baurine/instamaterial/ui/activity/PublishPhotoActivity.java
+++ app/src/main/java/com/baurine/instamaterial/ui/activity/PublishPhotoActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
 import com.baurine.instamaterial.R;
 
 public class PublishPhotoActivity extends BaseActivity {
@@ -18,6 +22,40 @@ public class PublishPhotoActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish_photo);
+
+        updateToolbar();
+    }
+
+    private void updateToolbar() {
+        getToolbar().setNavigationIcon(R.mipmap.ic_arrow_back_grey600_24dp);
+        getToolbar().setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    protected boolean needDrawer() {
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_publish_photo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_publish_photo) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
